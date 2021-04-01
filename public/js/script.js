11df78e36d8d45c7ac4f88ff7e2f34d03b919dc3
Denis Felipe <denisfelipedev@gmail.com>
2021-04-01 11:20:47 -0300

    Finalazido
    Projeto finalizado, com responsvidade e tema claro/escuro.

--- public/js/script.js
+++ public/js/script.js
@@ -92,3 +92,21 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
+//scroll reveal animation
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 1750,
+    reset: true
+})
+
+sr.reveal(`.home__data, .home__img, 
+           .about__data, .about__img,
+           .services__content, .menu__content,
+           .app__data, .app__img,
+           .contact__data, .contact__button,
+           .footer__content`, {
+    interval: 200
+})
+
