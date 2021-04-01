78616087c08f5a7345533678003746366d93ed5f
Denis Felipe <denisfelipedev@gmail.com>
2021-03-31 23:17:45 -0300

    Ajuste no js
    nada demais

--- public/js/script.js
+++ public/js/script.js
@@ -4,9 +4,8 @@ const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
-    // vaidate that variable exists
+    // vaidate if the variable exist
     if (toggle && nav) {
-        //Add the show-menu class to the div tag with the nav__menu class
         toggle.addEventListener('click', () => {
             nav.classList.toggle('show-menu')
         })
@@ -21,7 +20,6 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
     const navMenu = document.getElementById('nav-menu')
-    //when we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove ('show-menu')
 }
 
@@ -54,15 +52,14 @@ window.addEventListener('scroll', scrollActive)
 
 function scrollHeader(){
     const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
     if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL TOP ====================*/ 
+//show scroll top Button
+
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
