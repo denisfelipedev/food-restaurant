62d02c65a7db67217d7b3e72c36c4f68fc6b446d
Denis Felipe <denisfelipedev@gmail.com>
2021-04-01 11:00:53 -0300

    dark-theme configurado
    Feito a configuração do dark/light-theme com js

--- public/js/script.js
+++ public/js/script.js
@@ -64,4 +64,31 @@ function scrollTop(){
 }
 window.addEventListener('scroll', scrollTop)
 
+// Dark light theme
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+//set up the user's preference theme
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark':'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon':'bx-sun'
+
+if(selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+//dark light function
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
