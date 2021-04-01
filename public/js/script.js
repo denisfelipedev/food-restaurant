64dee062948110eee4ee42289045a2a3b94a30f4
Denis Felipe <denisfelipedev@gmail.com>
2021-03-31 23:16:22 -0300

    Iniciando o Dark-mode
    Corrigido alguns bugs do javascript e dabdi incio ao dark-mode.

--- public/js/script.js
+++ public/js/script.js
@@ -29,7 +29,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //Scroll actions active link
 
-const sections = document.querySelector('section[id]')
+const sections = document.querySelectorAll('section[id]')
 
 function scrollActive() {
     const scrollY = window.pageYOffset
@@ -37,13 +37,13 @@ function scrollActive() {
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
-        sectoinId = current.getAttribute('id')
+        sectionId = current.getAttribute('id')
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('nav__menu a [href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
         }
         else {
-            document.querySelector('nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
@@ -63,7 +63,7 @@ window.addEventListener('scroll', scrollHeader)
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
